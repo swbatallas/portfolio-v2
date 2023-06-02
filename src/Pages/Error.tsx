59896169f760c76e55e0a0301f9af1da78f88bdd
Ulyssbat <stewartbatallasminda@gmail.com>
2023-06-02 18:29:07 +0200

    Fix some typescript problems

--- src/Pages/Error.tsx
+++ src/Pages/Error.tsx
@@ -1,10 +1,8 @@
-import { Link, useRouteError } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Error() {
-  const error = useRouteError()
   return (
     <div className='h-screen text-slate-300 flex justify-center items-center flex-col'>
-      <h1>{error.message || error.statusText}</h1>
       <Link to={'/'}>Volver al Home</Link>
     </div>
   )
--- src/Pages/Fraggen.tsx
+++ src/Pages/Fraggen.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function Fraggen() {
   return (
     <div>Fraggen</div>
